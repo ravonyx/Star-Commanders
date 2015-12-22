9aedfc6f3f56caba307982742f88a1c6ce54ed47
ravonyx <ravonyxa@gmail.com>
2015-12-22 01:16:59 +0100

    Network Controller + anim (not in network)

---
+++ Assets/Scripts/Player/CameraController.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class CameraController : MonoBehaviour
+{
+
+    public float distCamera;
+    public float cameraSpeed = 1f;
+    public float smoothTime = 0.3f;
+
+    public Vector3 _offset;
+
+    private float _x = 0.0f;
+    private float _y = 0.0f;
+
+    public int _yMinLimit = 0;
+    public int _yMaxLimit = 70;
+
+    public int _xMinLimit = -90;
+    public int _xMaxLimit = 90;
+
+    private Vector3 _smoothTarget;
+    public GameObject target;
+
+    void LateUpdate()
+    {
+
+        if (target == null)
+            return;
+
+        float x = Input.GetAxis("Mouse X") * cameraSpeed;
+        float y = -Input.GetAxis("Mouse Y") * cameraSpeed;
+
+        _x += x * Time.deltaTime;
+        _y += y * Time.deltaTime;
+
+        _y = Clamp(_y, -90, 70);
+ 
+        transform.rotation = Quaternion.identity;
+        transform.Rotate(Vector3.up * _x);
+        transform.Rotate(Vector3.right * _y);
+
+        RotateCharacter(x);
+    }
+
+    float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360)
+            angle += 360;
+        if (angle > 360)
+            angle -= 360;
+        return Clamp(angle, min, max);
+    }
+
+    void RotateCharacter(float x)
+    {
+        _x += x * Time.deltaTime;
+        target.transform.rotation = Quaternion.identity;
+        target.transform.Rotate(Vector3.up * _x);
+    }
+
+    float Clamp(float value, float min , float max)
+    {
+         if(value<min)
+              value = min;
+         if(value > max)
+              value = max;
+         return value;
+    }
+}
---
+++ Assets/Scripts/Player/CharacController.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class CharacController : NetworkBehaviour
+{
+
+    private Animator anim;
+    private float jumpTimer = 0;
+
+    public float movementSpeed = 4.0f;
+    public float jumpSpeed = 4.0f;
+
+    private bool _wantToJump = false;
+    private Rigidbody _rigidbody;
+
+    private Vector3 _direction;
+    public Vector3 offset;
+
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponentInChildren<SkinnedMeshRenderer>().material.color = Color.red;
+        anim = this.gameObject.GetComponent<Animator>();
+        _rigidbody = GetComponent<Rigidbody>();
+        Camera.main.GetComponent<CameraController>().target = transform.gameObject;
+        var camera = Camera.main.transform;
+        camera.parent = transform;
+
+        Debug.Log(camera.localPosition);
+        camera.localPosition = offset;
+        Debug.Log(camera.localPosition);
+    }
+
+    void Update()
+    {
+        if (!isLocalPlayer)
+            return;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            _wantToJump = true;
+        if (Input.GetKey(KeyCode.LeftShift))
+            movementSpeed = 10.0f;
+        else
+            movementSpeed = 6.0f;
+
+    }
+
+    void FixedUpdate()
+    {
+        if (!isLocalPlayer)
+            return;
+
+        _direction = Vector3.zero;
+        if (Input.GetKey("z"))
+            _direction += transform.forward;
+        if (Input.GetKey("s"))
+            _direction -= transform.forward;
+        if (Input.GetKey("q"))
+            _direction -= transform.right;
+        if (Input.GetKey("d"))
+            _direction += transform.right;
+
+        _direction.Normalize();
+        _direction *= movementSpeed;
+
+        Vector3 groundDir = -Vector3.up;
+        float groundDist = 0.2f;
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position + new Vector3(0, 0.2f, 0), groundDir, out hit, groundDist))
+        {
+            if (_wantToJump)
+            {
+                _wantToJump = false;
+                jumpTimer = 1;
+                anim.SetBool("Jumping", true);
+
+            }
+        }
+        if (jumpTimer > 0.5) jumpTimer -= Time.deltaTime;
+        else if (anim.GetBool("Jumping") == true) anim.SetBool("Jumping", false);
+
+        _direction.y += _rigidbody.velocity.y;
+
+        _rigidbody.velocity = _direction;
+        float dotProduct = Vector3.Dot(_rigidbody.velocity, transform.right);
+        Vector3 velocity = new Vector3(_rigidbody.velocity.x, 0.0f, _rigidbody.velocity.z);
+
+
+
+        if (velocity.magnitude > 0)
+            anim.SetInteger("Speed", 2);
+        else
+            anim.SetInteger("Speed", 0);
+
+    }
+}
