e236835e8b1167877efa89c4f148181a77187dcb
ravonyx <ravonyxa@gmail.com>
2016-03-01 22:29:59 +0100

    Add new scripts for network / new scene MenuScene (= Lobby) / new prefab for UI proto

---
+++ Assets/Scripts/Network/NetworkManager.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using Photon;
+
+public class NetworkManager : Photon.PunBehaviour
+{
+
+    public Text connectionParameters;
+    public Text roomName;
+    private bool _inRoom = false;
+    public Canvas canvas;
+    private ListRooms _listRooms;
+
+    void Start ()
+    {
+        PhotonNetwork.ConnectUsingSettings("0.1");
+        PhotonNetwork.logLevel = PhotonLogLevel.Full;
+        _listRooms = canvas.GetComponent<ListRooms>();
+    }
+
+	void Update ()
+    {
+        connectionParameters.text = PhotonNetwork.connectionStateDetailed.ToString();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        //PhotonNetwork.JoinRandomRoom();
+    }
+
+    public void CreateRoom()
+    {
+        if(!_inRoom)
+            PhotonNetwork.CreateRoom(roomName.text, new RoomOptions() { maxPlayers = 4 }, null);
+        _inRoom = true;
+    }
+
+    void OnPhotonRandomJoinFailed()
+    {
+        Debug.Log("Can't join random room!");
+    }
+
+    public void JoinRoom()
+    {
+        PhotonNetwork.JoinRoom(_listRooms.selectedRoomName);
+    }
+    
+    public override void OnJoinedRoom()
+    {
+         GameObject player = PhotonNetwork.Instantiate("playerPrefab", Vector3.zero, Quaternion.identity, 0);
+         CharacController controller = player.GetComponent<CharacController>();
+         controller.enabled = true;
+         player.GetComponent<Rigidbody>().isKinematic = false;
+         CameraController cam = Camera.main.GetComponent<CameraController>();
+         GameObject cameraObj = Camera.main.gameObject;
+         controller.cameraObj = cameraObj;
+         cam.enabled = true;
+         canvas.enabled = false;
+    }
+}
---
+++ Assets/Scripts/Network/NetworkPlayer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using Photon;
+
+public class NetworkPlayer : Photon.MonoBehaviour
+{
+    private Vector3 correctPlayerPos = Vector3.zero;
+    private Quaternion correctPlayerRot = Quaternion.identity;
+
+    void Update()
+    {
+        if (!photonView.isMine)
+        {
+            transform.position = Vector3.Lerp(transform.position, correctPlayerPos, Time.deltaTime * 5);
+            transform.rotation = Quaternion.Lerp(transform.rotation, correctPlayerRot, Time.deltaTime * 5);
+        }
+    }
+
+    void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            // We own this player: send the others our data
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+        }
+        else
+        {
+            // Network player, receive data
+            correctPlayerPos = (Vector3)stream.ReceiveNext();
+            correctPlayerRot = (Quaternion)stream.ReceiveNext();
+        }
+    }
+}
