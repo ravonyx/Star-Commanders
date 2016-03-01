4c6732d7bb2309850d357c112db55a6f22687b98
ravonyx <ravonyxa@gmail.com>
2016-03-01 23:46:18 +0100

    Add scripts for instantitate on start/load scene + add MainScene with screen and turret + add serverbutton for listrooms in menuscene

---
+++ Assets/Scripts/Network/InstantiateOnLoad.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InstantiateOnLoad : MonoBehaviour
+{
+    public string namePrefab;
+    public bool network;
+
+	void Start ()
+    {
+        GameObject player = null;
+
+        if (network)
+            player = PhotonNetwork.Instantiate(namePrefab, Vector3.zero, Quaternion.identity, 0);
+        else
+            player = GameObject.Instantiate(Resources.Load(namePrefab), Vector3.zero, Quaternion.identity) as GameObject;
+        if (player)
+        {
+            CharacController controller = player.GetComponent<CharacController>();
+            controller.enabled = true;
+            player.GetComponent<Rigidbody>().isKinematic = false;
+            CameraController cam = Camera.main.GetComponent<CameraController>();
+            GameObject cameraObj = Camera.main.gameObject;
+            controller.cameraObj = cameraObj;
+            cam.enabled = true;
+        }
+        else
+            Debug.Log("Add " + namePrefab + " in folder Resources");
+    }
+
+}
