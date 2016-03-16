ffd03a1ab8d77df252c69da1fe7f78f349ccd1f7
unknown <MMD2@DELL1937DSY.dsone.3ds.com>
2016-03-16 18:10:55 +0100

    Object network general script

---
+++ Assets/Scripts/Network/ObjectNetwork.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using Photon;
+
+public class ObjectNetwork : Photon.MonoBehaviour
+{
+    private Vector3 objectPos = Vector3.zero;
+    private Quaternion objectRot = Quaternion.identity;
+    PhotonView view;
+
+    void Start()
+    {
+        int viewID = PhotonNetwork.AllocateViewID();
+        view = GetComponent<PhotonView>();
+        view.viewID = viewID;
+
+        objectPos = transform.position;
+    }
+
+    void Update()
+    {
+        if (!view.isMine)
+        {
+             transform.position = Vector3.Lerp(transform.position, objectPos, Time.deltaTime * 15);
+             transform.rotation = Quaternion.Lerp(transform.rotation, objectRot, Time.deltaTime * 20);
+        }
+    }
+
+    void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            // We own this player: send the others our data
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+        }
+        else
+        {
+            // Network player, receive data
+            objectPos = (Vector3)stream.ReceiveNext();
+            objectRot = (Quaternion)stream.ReceiveNext();
+        }
+    }
+}
