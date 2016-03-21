8404bb379084c37668f8be2a652a7b50b1dffefd
Valentin_hayot_work <pornin@hotmail.fr>
2016-03-21 10:49:19 +0100

    Energy Shield update
    - Shield shader added
    - Shield manager added (shieldMax and callback for weapon impact)
    - Shield impact effect added
    - /!\ Use collision system /!\ so do not forget rigidbody on bullet

---
+++ Assets/Scripts/SpaceshipComponents/ShieldController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class ShieldController : MonoBehaviour {
+
+    [SerializeField]
+    private int m_ShieldMax;
+
+    private int m_shieldLevel;
+    // Use this for initialization
+    void Start () {
+        m_shieldLevel = m_ShieldMax;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+   public void ImpactReceived(GameObject go)
+    {
+        if (go.tag == "bullet")
+        {
+            m_shieldLevel--;
+            Debug.Log(m_shieldLevel);
+        }
+    }
+}
---
+++ Assets/Scripts/SpaceshipComponents/ShieldEffect.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShieldEffect : MonoBehaviour
+{
+    public float EffectTime;
+    private UnityEngine.Color ShieldColor;
+    private UnityEngine.Color tempColor;
+    [SerializeField] private ShieldController m_impactCallback;
+    void Start()
+    {
+        ShieldColor = GetComponent<Renderer>().material.GetColor("_ShieldColor");
+        tempColor = ShieldColor;
+        tempColor.a = 0.05f;
+    }
+
+    void Update()
+    {
+        if (EffectTime > 0)
+        {
+            if (EffectTime < 450 && EffectTime > 400)
+            {
+                GetComponent<Renderer>().material.SetVector("_ShieldColor", ShieldColor);
+            }
+
+            EffectTime -= Time.deltaTime * 1000;
+            GetComponent<Renderer>().material.SetVector("_Position", transform.FindChild("hitpoint").position);
+            GetComponent<Renderer>().material.SetFloat("_EffectTime", EffectTime);
+        }
+    }
+
+    void  OnCollisionEnter(Collision collision)
+    {
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            //Debug.Log("contact");
+            GetComponent<Renderer>().material.SetVector("_ShieldColor", tempColor);
+            transform.FindChild("hitpoint").position = contact.point;
+            EffectTime = 500;
+            m_impactCallback.ImpactReceived(collision.gameObject);
+        }
+    }
+}
