4f8ba5ce75ac635babe733f5bc5a165397226fa7
ravonyx <ravonyxa@gmail.com>
2016-03-22 22:53:24 +0100

    Shied network event

--- Assets/Scripts/SpaceshipComponents/ShieldController.cs
+++ Assets/Scripts/SpaceshipComponents/ShieldController.cs
@@ -18,52 +18,55 @@ public class ShieldController : MonoBehaviour {
     private int m_RearLeftLevel;
     private int m_RearRightLevel;
 
-    // Use this for initialization
-    void Start () {
+    void Awake()
+    {
+        PhotonNetwork.OnEventCall += this.OnEvent;
+    }
 
+    void Start ()
+    {
         m_FrontLeftLevel = m_FrontLeftMax;
         m_FrontRightLevel = m_FrontRightMax;
         m_RearLeftLevel = m_RearLeftMax;
         m_RearRightLevel = m_RearRightMax;
     }
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 
    public void FrontLeftImpact(GameObject go)
     {
-        if (go.tag == "bullet")
+        if (go.tag == "bullet" && PhotonNetwork.isMasterClient)
         {
             m_FrontLeftLevel--;
+            PhotonNetwork.RaiseEvent(0, m_FrontLeftLevel, true, null);
             Debug.Log("m_FrontLeftLevel " + m_FrontLeftLevel);
         }
     }
 
     public void FrontRightImpact(GameObject go)
     {
-        if (go.tag == "bullet")
+        if (go.tag == "bullet" && PhotonNetwork.isMasterClient)
         {
             m_FrontRightLevel--;
+            PhotonNetwork.RaiseEvent(1, m_FrontRightLevel, true, null);
             Debug.Log("m_FrontRightLevel " + m_FrontRightLevel);
         }
     }
 
     public void RearleftImpact(GameObject go)
     {
-        if (go.tag == "bullet")
+        if (go.tag == "bullet" && PhotonNetwork.isMasterClient)
         {
             m_RearLeftLevel--;
+            PhotonNetwork.RaiseEvent(2, m_RearLeftLevel, true, null);
             Debug.Log("m_RearLeftLevel " + m_RearLeftLevel);
         }
     }
 
     public void RearRightImpact(GameObject go)
     {
-        if (go.tag == "bullet")
+        if (go.tag == "bullet" && PhotonNetwork.isMasterClient)
         {
             m_RearRightLevel--;
+            PhotonNetwork.RaiseEvent(3, m_RearRightLevel, true, null);
             Debug.Log("m_RearRightLevel " + m_RearRightLevel);
         }
     }
@@ -84,5 +87,42 @@ public class ShieldController : MonoBehaviour {
                 return -1;
         }
 
+    }
+
+    // handle events:
+    private void OnEvent(byte eventcode, object content, int senderid)
+    {
+        Debug.Log("event");
+        if (eventcode == 0)
+        {
+            PhotonPlayer sender = PhotonPlayer.Find(senderid);  // who sent this?
+            Debug.Log("Sender : " + sender);
+            Debug.Log("content : " + content);
+            m_FrontLeftLevel = (int)content;
+        }
+
+        else if (eventcode == 1)
+        {
+            PhotonPlayer sender = PhotonPlayer.Find(senderid);  // who sent this?
+            Debug.Log("Sender : " + sender);
+            Debug.Log("content : " + content);
+            m_FrontRightLevel = (int)content;
+        }
+
+       else if (eventcode == 2)
+        {
+            PhotonPlayer sender = PhotonPlayer.Find(senderid);  // who sent this?
+            Debug.Log("Sender : " + sender);
+            Debug.Log("content : " + content);
+            m_RearLeftLevel = (int)content;
+        }
+
+        if (eventcode == 3)
+        {
+            PhotonPlayer sender = PhotonPlayer.Find(senderid);  // who sent this?
+            Debug.Log("Sender : " + sender);
+            Debug.Log("content : " + content);
+            m_RearRightLevel = (int)content;
+        }
     }
 }
