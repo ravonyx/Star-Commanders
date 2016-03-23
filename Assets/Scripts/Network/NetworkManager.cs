3f87e3cdc5730d4ca4e0741687ab8f27170e2a61
ravonyx <ravonyxa@gmail.com>
2016-03-23 21:10:47 +0100

    Menu finished

--- Assets/Scripts/Network/NetworkManager.cs
+++ Assets/Scripts/Network/NetworkManager.cs
@@ -9,7 +9,10 @@ public class NetworkManager : Photon.PunBehaviour
     public Text connectionParameters;
     public Text roomName;
     public Text feedback;
-    public Text nbPlayersText;
+    public Text nbPlayersInRoom;
+    public Text nbPlayersWanted;
+    public Button playButton;
+
     private bool _inRoom = false;
     public Canvas canvas;
     private ListRooms _listRooms;
@@ -34,10 +37,13 @@ public class NetworkManager : Photon.PunBehaviour
     {
         if(connectionParameters)
             connectionParameters.text = PhotonNetwork.connectionStateDetailed.ToString();
+        playButton.gameObject.SetActive(false);
         if (PhotonNetwork.inRoom)
-            nbPlayersText.text = PhotonNetwork.playerList.Length + "/" + nbPlayers.ToString(); 
-        if (Input.GetKey(KeyCode.Space)) //For debug only
-            this.photonView.RPC("LoadSceneForEach", PhotonTargets.All);
+        {
+            nbPlayersInRoom.text = PhotonNetwork.room.playerCount + "/" + PhotonNetwork.room.maxPlayers;
+            if (PhotonNetwork.isMasterClient)
+                playButton.gameObject.SetActive(true);
+        }
         if (_isFade)
         {
             float timeRatio = _startTime / _fadeTime;
@@ -56,6 +62,12 @@ public class NetworkManager : Photon.PunBehaviour
         }
     }
 
+    public void Play()
+    {
+        if(PhotonNetwork.inRoom && int.Parse(nbPlayersWanted.text) == PhotonNetwork.playerList.Length)
+            this.photonView.RPC("LoadSceneForEach", PhotonTargets.All);
+    }
+
     [PunRPC]
     void LoadSceneForEach(PhotonMessageInfo info)
     {
@@ -64,18 +76,35 @@ public class NetworkManager : Photon.PunBehaviour
 
     public void CreateRoom()
     {
-        //int nb = int.Parse(nbPlayers.text);
-        byte value = (byte)nbPlayers;
-        if (!PhotonNetwork.inRoom && roomName.text != "")
+        if (nbPlayersWanted.text != "" && roomName.text != "")
         {
-            PhotonNetwork.CreateRoom(roomName.text, new RoomOptions() { maxPlayers = value }, null);
-            menuScript.GoToPanel(waitPanel);
-            _inRoom = true;
+            int nb = int.Parse(nbPlayersWanted.text);
+            byte value = (byte)nb;
+            if (!PhotonNetwork.inRoom && PhotonNetwork.insideLobby && nb > 0)
+            {
+                PhotonNetwork.CreateRoom(roomName.text, new RoomOptions() { maxPlayers = value }, null);
+                menuScript.GoToPanel(waitPanel);
+                _inRoom = true;
+            }
+            else if(nb <= 0)
+            {
+                feedback.text = "Number < 0";
+                Color color = new Color(feedback.color.r, feedback.color.g, feedback.color.b, 1.0f);
+                feedback.color = color;
+                _isFade = true;
+                _startTime = 0.0f;
+                _fadeTime = 2.0f;
+            }
         }
-        else if (roomName.text == "")
+        else
         {
-            Debug.Log("Room name empty");
-            feedback.text = "Enter a room name";
+            if (nbPlayersWanted.text == "" && roomName.text == "")
+                feedback.text = "Fill the form";
+            else if (nbPlayersWanted.text == "")
+                feedback.text = "Enter number of players";
+            else if (roomName.text == "")
+                feedback.text = "Enter a room name";
+
             Color color = new Color(feedback.color.r, feedback.color.g, feedback.color.b, 1.0f);
             feedback.color = color;
             _isFade = true;
@@ -97,6 +126,21 @@ public class NetworkManager : Photon.PunBehaviour
 
     public void JoinRoom()
     {
-        PhotonNetwork.JoinRoom(_listRooms.selectedRoomName);
+        if(_listRooms.selectedRoomName != "")
+        {
+            PhotonNetwork.JoinRoom(_listRooms.selectedRoomName);
+            menuScript.GoToPanel(waitPanel);
+            _inRoom = true;
+        }
+        else
+        {
+            feedback.text = "Select room";
+
+            Color color = new Color(feedback.color.r, feedback.color.g, feedback.color.b, 1.0f);
+            feedback.color = color;
+            _isFade = true;
+            _startTime = 0.0f;
+            _fadeTime = 2.0f;
+        }
     }
 }
