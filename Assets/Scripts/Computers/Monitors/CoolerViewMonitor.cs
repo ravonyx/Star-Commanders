d804207beed1d029825fa7087dc37b7f085d209b
Kevin Rey <k.rey@hotmail.fr>
2016-03-24 02:26:55 +0100

    Ajout écrans dans la salle de contrôle
    - Ecran Bouclier
    - Ecran Propulseur
    - Ecran Tourelle
    - Ecran Cooler

---
+++ Assets/Scripts/Computers/Monitors/CoolerViewMonitor.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CoolerViewMonitor : MonoBehaviour 
+{
+    [SerializeField]
+    CoolingUnitController _coolerCont;
+
+    [SerializeField]
+    Image[] _coolerImages;
+
+    [SerializeField]
+    Text[] _coolerStates;
+
+    void Start()
+    {
+        InvokeRepeating("UpdateInterface", 1.0f, 2.0f);
+    }
+
+
+    void UpdateInterface()
+    {
+        for (int id = 0; id < 6; id++)
+        {
+            int lifeCooler = _coolerCont.GetCoolingUnitLifeLevel(id + 1);
+            _coolerStates[id].text = lifeCooler + "/100";
+            _coolerImages[id].color = new Color((100.0f - lifeCooler) / 100.0f, lifeCooler / 100.0f, 0.0f, 1.0f);
+        }
+    }
+}
