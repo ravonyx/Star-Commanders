bbb561d516b3e449ab4218e15523742971f058a4
ravonyx <ravonyxa@gmail.com>
2016-03-24 23:29:55 +0100

    move camera on workshop

--- Assets/Scripts/Player/CharacController.cs
+++ Assets/Scripts/Player/CharacController.cs
@@ -41,50 +41,52 @@ public class CharacController : Photon.MonoBehaviour
 
     void FixedUpdate()
     {
-        if (_isActiveView && photonView.isMine)
+        if(photonView.isMine)
         {
-            _direction = Vector3.zero;
-            if (Input.GetKey("z"))
-                _direction += transform.forward;
-            if (Input.GetKey("s"))
-                _direction -= transform.forward;
-            if (Input.GetKey("q"))
-                _direction -= transform.right;
-            if (Input.GetKey("d"))
-                _direction += transform.right;
-
-            _direction.Normalize();
-            _direction *= movementSpeed;
-
-            Vector3 groundDir = -Vector3.up;
-            float groundDist = 0.2f;
-            RaycastHit hit;
-            Debug.DrawRay(transform.position, new Vector3(0, -groundDist, 0), Color.red, 2.0f, true);
-
-            int layerMask = 1 << 8;
-            layerMask = ~layerMask;
-            if (_wantToJump && Physics.Raycast(transform.position + new Vector3(0.0f,0.2f,0.0f), groundDir, out hit, groundDist, layerMask))
+            if (_isActiveView)
             {
+                _direction = Vector3.zero;
+                if (Input.GetKey("z"))
+                    _direction += transform.forward;
+                if (Input.GetKey("s"))
+                    _direction -= transform.forward;
+                if (Input.GetKey("q"))
+                    _direction -= transform.right;
+                if (Input.GetKey("d"))
+                    _direction += transform.right;
+
+                _direction.Normalize();
+                _direction *= movementSpeed;
+
+                Vector3 groundDir = -Vector3.up;
+                float groundDist = 0.2f;
+                RaycastHit hit;
+                Debug.DrawRay(transform.position, new Vector3(0, -groundDist, 0), Color.red, 2.0f, true);
+
+                int layerMask = 1 << 8;
+                layerMask = ~layerMask;
+                if (_wantToJump && Physics.Raycast(transform.position + new Vector3(0.0f, 0.2f, 0.0f), groundDir, out hit, groundDist, layerMask))
+                {
                     _wantToJump = false;
                     jumpTimer = 1;
                     anim.SetBool("Jumping", true);
                     _direction += transform.up * jumpSpeed;
+                }
+                transform.rotation = Quaternion.FromToRotation(transform.up, spaceship.transform.up) * transform.rotation;
+
+                _rigidbody.velocity = _direction;
+                _rigidbody.AddForce(gravity * spaceship.transform.up);
+
+                Vector3 velocity = new Vector3(_rigidbody.velocity.x, 0.0f, _rigidbody.velocity.z);
+                if (velocity.magnitude > 0.25f)
+                    anim.SetInteger("Speed", 2);
+                else
+                    anim.SetInteger("Speed", 0);
+
+                if (jumpTimer > 0.5) jumpTimer -= Time.deltaTime;
+                else if (anim.GetBool("Jumping") == true) anim.SetBool("Jumping", false);
             }
-
             transform.Rotate(Vector3.up * (Input.GetAxis("Mouse X") * 5));
-            transform.rotation = Quaternion.FromToRotation(transform.up, spaceship.transform.up) * transform.rotation;
-
-            _rigidbody.velocity = _direction;
-            _rigidbody.AddForce(gravity * spaceship.transform.up);
-
-            Vector3 velocity = new Vector3(_rigidbody.velocity.x, 0.0f, _rigidbody.velocity.z);
-            if (velocity.magnitude > 0.25f)
-                anim.SetInteger("Speed", 2);
-            else
-                anim.SetInteger("Speed", 0);
-
-            if (jumpTimer > 0.5) jumpTimer -= Time.deltaTime;
-            else if (anim.GetBool("Jumping") == true) anim.SetBool("Jumping", false);
         }
     }
 }
