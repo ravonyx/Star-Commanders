a3a571a25339cf861b11ce10b0fc44b3a9aac9ff
ravonyx <ravonyxa@gmail.com>
2016-03-28 20:27:05 +0200

    Fix test in main scene

--- Assets/Scripts/Network/InstantiateOnLoad.cs
+++ Assets/Scripts/Network/InstantiateOnLoad.cs
@@ -12,41 +12,60 @@ public class InstantiateOnLoad : Photon.MonoBehaviour
     public GameObject spaceship;
 
     [SerializeField]
-    private List<GameObject> _players;
-
-    [SerializeField]
     private Text _playerInfo;
 
     void Start ()
     {
         GameObject player = null;
-        GameObject cameraObj = null;
-
-        if (network)
-            player = PhotonNetwork.Instantiate(namePrefab, position, Quaternion.identity, 0);
-        else
-            player = GameObject.Instantiate(Resources.Load(namePrefab), position, Quaternion.identity) as GameObject;
-
-        if (player)
+        if(network)
         {
-            cameraObj = GameObject.Instantiate(Resources.Load("MainCamera"), Vector3.zero, Quaternion.identity) as GameObject;
-            cameraObj.transform.parent = player.transform;
-            cameraObj.transform.localPosition = offset;
-            CameraController cam = cameraObj.GetComponent<CameraController>();
-            cam.target = player.transform.gameObject;
-
-            RaycastInteraction raycast = player.GetComponent<RaycastInteraction>();
-            raycast.camController = cam;
-            raycast._playerInfo = _playerInfo;
-
-            if(!network)
-            {
-                player.GetComponent<NetworkPlayer>().enabled = false;
-                player.GetComponent<PhotonAnimatorView>().enabled = false;
-            }
-            player.transform.parent = spaceship.transform;
+            player = PhotonNetwork.Instantiate(namePrefab, position, Quaternion.identity, 0);
+            if (player)
+                InitPlayer(player);
+            else
+                Debug.Log("Add " + namePrefab + " in folder Resources");
         }
         else
-            Debug.Log("Add " + namePrefab + " in folder Resources");
+        {
+            PhotonNetwork.ConnectUsingSettings("v0.1");
+        }
+    }
+
+    private void InitPlayer(GameObject player)
+    {
+        GameObject cameraObj = null;
+
+        cameraObj = GameObject.Instantiate(Resources.Load("MainCamera"), Vector3.zero, Quaternion.identity) as GameObject;
+        cameraObj.transform.parent = player.transform;
+        cameraObj.transform.localPosition = offset;
+        CameraController cam = cameraObj.GetComponent<CameraController>();
+        cam.target = player.transform.gameObject;
+
+        RaycastInteraction raycast = player.GetComponent<RaycastInteraction>();
+        raycast.camController = cam;
+        raycast._playerInfo = _playerInfo;
+
+        player.transform.parent = spaceship.transform;
+    }
+    void OnJoinedLobby()
+    {
+        if(!network)
+        {
+            Debug.Log("JoinedLobby");
+            PhotonNetwork.CreateRoom("test", new RoomOptions() { maxPlayers = 1 }, null);
+
+        }
+    }
+    void OnJoinedRoom()
+    {
+        if (!network)
+        {
+            Debug.Log("JoinedRoom");
+            GameObject player = null;
+            player = PhotonNetwork.Instantiate(namePrefab, position, Quaternion.identity, 0);
+            if (player)
+                InitPlayer(player);
+
+        }
     }
 }
