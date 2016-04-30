1446288a13af6a71aef969ca094e06dc46814792
ravonyx <ravonyxa@gmail.com>
2016-04-30 15:26:23 +0200

    Commit option working

--- Assets/Scripts/Menu/SettingsScript.cs
+++ Assets/Scripts/Menu/SettingsScript.cs
@@ -1,16 +1,36 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
 
 public class SettingsScript : MonoBehaviour
 {
     [SerializeField]
-    Slider VolumeSlider;
+    private Dropdown qualityDropdown;
     [SerializeField]
-    Slider QualitySlider;
+    private Dropdown screenDropdown;
+    [SerializeField]
+    Slider VolumeSlider;
     [SerializeField]
     Toggle AntiAlias;
 
+    void Start()
+    {
+        Debug.Log("Start");
+        for(int i = 0; i < Screen.resolutions.Length; i++)
+            screenDropdown.options.Add(new Dropdown.OptionData() { text = Screen.resolutions[i].width + " X " + Screen.resolutions[i].height});
+        screenDropdown.GetComponentInChildren<Text>().text = Screen.width + " X " + Screen.height;
+
+        for (int i = 0; i < QualitySettings.names.Length; i++)
+            qualityDropdown.options.Add(new Dropdown.OptionData() { text = QualitySettings.names[i] });
+        qualityDropdown.GetComponentInChildren<Text>().text = QualitySettings.names[QualitySettings.GetQualityLevel()];
+    }
+
+    public void SetResolution()
+    {
+        Debug.Log(screenDropdown.value);
+        Screen.SetResolution(Screen.resolutions[screenDropdown.value].width, Screen.resolutions[screenDropdown.value].height, Screen.fullScreen);
+    }
 
     public void SetVolume(float new_volume)
     {
@@ -18,11 +38,10 @@ public class SettingsScript : MonoBehaviour
         AudioListener.volume = new_volume;
     }
 
-    public void SetQuality(float new_quality)
+    public void SetQuality()
     {
-        Debug.Log(QualitySlider.value);
-
-        QualitySettings.SetQualityLevel((int)new_quality, false);
+        Debug.Log(qualityDropdown.value);
+        QualitySettings.SetQualityLevel((int)qualityDropdown.value, true);
     }
 
     public void SetAntiAlias(bool activated)
