0548d228aab4b8818ed6dca7bdb607b3dc3ca2f2
ravonyx <ravonyxa@gmail.com>
2016-05-01 18:03:01 +0200

    exit zone repop + raycast off window + place spawn spaceship

--- Assets/Scripts/ExitZone.cs
+++ Assets/Scripts/ExitZone.cs
@@ -2,12 +2,16 @@
 using System.Collections;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
 
 public class ExitZone : Photon.MonoBehaviour
 {
     public Text warningText;
+    public List<GameObject> spaceshipsOut;
+    public Transform spawnPosBlue;
+    public Transform spawnPosRed;
 
-	void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider other)
     {
         if (PhotonNetwork.isMasterClient && other.tag == "Hull")
             photonView.RPC("StopCountdown", PhotonTargets.All, PhotonNetwork.player.GetTeam().ToString());
@@ -16,23 +20,31 @@ public class ExitZone : Photon.MonoBehaviour
     void OnTriggerExit(Collider other)
     {
         if (PhotonNetwork.isMasterClient && other.tag == "Hull")
-            photonView.RPC("StartCountdown", PhotonTargets.All, PhotonNetwork.player.GetTeam().ToString());
+            photonView.RPC("StartCountdown", PhotonTargets.All, PhotonNetwork.player.GetTeam().ToString(), other.transform.root.gameObject.GetComponent<PhotonView>().viewID);
     }
 
-    IEnumerator CountdownExitZone()
+    IEnumerator CountdownExitZone(object[] parms)
     {
-        yield return new WaitForSeconds(20);
-        warningText.text = "You lose";
+        yield return new WaitForSeconds(5);
+        warningText.text = "Repop ! ";
         yield return new WaitForSeconds(1);
-        SceneManager.LoadScene("Menu");
+
+        GameObject spaceship = (GameObject)parms[0];
+        if ((string)parms[1] == PunTeams.Team.blue.ToString())
+            spaceship.transform.position = spawnPosBlue.position;
+        else
+            spaceship.transform.position = spawnPosRed.position;
+        warningText.text = "";
     }
 
     [PunRPC]
-    void StartCountdown(string team)
+    void StartCountdown(string team, int spaceshipID)
     {
+        GameObject spaceship = PhotonView.Find(spaceshipID).gameObject;
+        object[] parms = new object[2] { spaceship, team };
         if (PhotonNetwork.player.GetTeam().ToString() == team)
         {
-            StartCoroutine("CountdownExitZone");
+            StartCoroutine("CountdownExitZone", parms);
             warningText.text = "Exit Zone !!";
         }
     }
