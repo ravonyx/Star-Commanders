ceb85fd409d50087a4710f9c92aacb371c315a13
Maillard Manon <Maillard Manon>
2016-05-18 20:10:25 +0200

    add sound on click

--- Assets/Scripts/UI/SoundUI.cs
+++ Assets/Scripts/UI/SoundUI.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class SoundUI : MonoBehaviour
 {
     public AudioClip soundHover;
+    public AudioClip soundCLick;
     AudioSource audio;
 
     void Start()
@@ -14,5 +15,9 @@ public class SoundUI : MonoBehaviour
     public void Hover()
     {
         audio.PlayOneShot(soundHover, 0.5F);
+    }
+    public void Click()
+    {
+        audio.PlayOneShot(soundCLick, 0.5F);
     }
 }
