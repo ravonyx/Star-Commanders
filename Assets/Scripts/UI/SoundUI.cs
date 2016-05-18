17642b5901adf50322ecaf6c140af844c7dbbcf3
Manon Maillard <ravonyxa@gmail.com>
2016-05-18 13:53:01 +0200

    add sounds on button

---
+++ Assets/Scripts/UI/SoundUI.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundUI : MonoBehaviour
+{
+    public AudioClip soundHover;
+    AudioSource audio;
+
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    public void Hover()
+    {
+        audio.PlayOneShot(soundHover, 0.5F);
+    }
+}
